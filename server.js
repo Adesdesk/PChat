a4bc0c585471b3123c21206f6e92e4e19a24964b
Adeola David Adelakun <aadelakun28@gmail.com>
2023-05-07 21:57:39 +0100

    Model.find() syntax replaced

--- server.js
+++ server.js
@@ -1,45 +1,57 @@
 require('dotenv').config();
-var express = require('express');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 const access = process.env.DATABASE_ACCESS;
 const dburl = "mongodb+srv://adesdeskUser01:" + access + "@adesdeskdatabase01.7znhfhj.mongodb.net/?retryWrites=true&w=majority";
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-var Message = mongoose.model('Message',{ name : String, message : String});
+const messageSchema = new mongoose.Schema({ 
+  name: String, 
+  message: String 
+});
+
+const Message = mongoose.model('Message', messageSchema);
 
 app.get('/socket.io/socket.io.js', (req, res) => {
   res.sendFile(__dirname + '/node_modules/socket.io/client-dist/socket.io.js');
 });
 
-
 app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
-})
+  Message.find({})
+    .then((messages) => {
+      res.send(messages);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
 
 app.post('/messages', (req, res) => {
-  var message = new Message(req.body);
-  message.save((err) =>{
-    if(err)
-      sendStatus(500);
+  const message = new Message(req.body);
+  message.save()
+    .then(() => {
       io.emit('message', req.body);
-    res.sendStatus(200);
-  })
-})
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
 
-io.on('connection', () =>{
-  console.log('a user is connected')
- })
+io.on('connection', () => {
+  console.log('a user is connected');
+});
 
 // Connect to the database
 mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -67,6 +79,6 @@ process.on('SIGINT', () => {
   });
 });
 
-var server = app.listen(5000, () => {
-    console.log('server is running on port', server.address().port);
-   });
+const server = app.listen(5000, () => {
+  console.log('server is running on port', server.address().port);
+});
